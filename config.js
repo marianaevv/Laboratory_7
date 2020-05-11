e5d68cdefef1b80114f29875e0c63e5563be063e
Marianavv <mariana2v@hotmail.com>
2020-05-11 01:14:40 -0500

    Connected to deployed database

---
+++ config.js
@@ -0,0 +1,4 @@
+exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/bookmarksdb';
+exports.TOKEN = process.env.TOKEN || '2abbf7c3-245b-404f-9473-ade729ed4653';
+exports.PORT = process.env.PORT || '8080';
+//mongodb+srv://admin:pass123@cluster0-haqhd.mongodb.net/bookmarksdb?retryWrites=true&w=majority
